52fb65412aa9fd6ca95e477a3be5a0b8015e8e87
Aftab77985 <aaftab_2013@yahoo.com>
2022-03-21 02:32:34 +0500

    some changes in preload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function switchControl(char) {
     switch (char) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.preload.play();
+            tom1.preload();
             tom1.play();
             break;
 
