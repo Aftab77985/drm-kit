9338b9659b7b671c7ece172ced5da4657cfc305a
Aftab77985 <aaftab_2013@yahoo.com>
2022-03-21 02:40:58 +0500

    preload issue

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ function switchControl(char) {
     switch (char) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.preload();
+            tom1.preload = "auto";
             tom1.play();
             break;
 
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.preload = "auto";
             tom2.play();
             break;
 
