6621cf61743e014c051f27eb3c8fdfbe2c69e33c
Aftab77985 <aaftab_2013@yahoo.com>
2022-03-21 02:46:12 +0500

    tried to preload audio files

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+function preLoadAudioFiles(){
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.preload = "auto";
+
+    var tom2 = new Audio("sounds/tom-2.mp3");
+    tom2.preload = "auto";
+
+    var tom3 = new Audio("sounds/tom-3.mp3");
+    tom3.preload = "auto";
+
+    var tom4 = new Audio("sounds/tom-4.mp3");
+    tom4.preload = "auto";
+
+    var snare = new Audio("sounds/snare.mp3");
+    snare.preload = "auto";
+
+    var kickBass = new Audio("sounds/kick-bass.mp3");
+    kickBass.preload = "auto";
+
+    var crash = new Audio("sounds/crash.mp3");
+    crash.preload = "auto";
+}
+preLoadAudioFiles();
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
     var button = document.querySelectorAll(".drum")[i];
@@ -17,13 +40,11 @@ function switchControl(char) {
     switch (char) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.preload = "auto";
             tom1.play();
             break;
 
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.preload = "auto";
             tom2.play();
             break;
 
